94b90d0fd44f1eec9f1c553e7bb51b23324d5658
Peter Thorson <github@zaphoyd.com>
2011-10-03 07:26:22 -0500

    preliminary work on an echo_client (to run against testing suites)

---
+++ examples/echo_client/echo_client.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2011, Peter Thorson. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the WebSocket++ Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL PETER THORSON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+#include "echo_client_handler.hpp"
+
+#include <websocketpp/websocketpp.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+#include <iostream>
+
+using boost::asio::ip::tcp;
+using namespace websocketecho;
+
+int main(int argc, char* argv[]) {
+	std::string uri;
+	
+	/*if (argc != 2) {
+		std::cout << "Usage: `echo_client test_url`" << std::endl;
+	} else {
+		uri = argv[1];
+	}*/
+	
+	echo_client_handler_ptr c(new echo_client_handler());
+	
+	try {
+		boost::asio::io_service io_service;
+		
+		websocketpp::client_ptr client(new websocketpp::client(io_service,c));
+		
+		client->init();
+		client->set_header("User Agent","WebSocket++/2011-09-25");
+		
+		client->connect("ws://localhost:9001/getCaseCount");
+		
+		io_service.run();
+		
+		std::cout << "done" << std::endl;
+		
+	} catch (std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+	
+	return 0;
+}
---
+++ examples/echo_client/echo_client_handler.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2011, Peter Thorson. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the WebSocket++ Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL PETER THORSON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+#include "echo_client_handler.hpp"
+
+#include <boost/algorithm/string/replace.hpp>
+
+using websocketecho::echo_client_handler;
+using websocketpp::client_session_ptr;
+
+void echo_client_handler::on_open(session_ptr s) {
+	std::cout << "Successfully connected: " << s->get_resource() << std::endl;
+}
+
+void echo_client_handler::on_close(session_ptr s,uint16_t status,const std::string &reason) {
+	std::cout << "client was disconnected" << std::endl;
+}
+
+void echo_client_handler::on_message(session_ptr s,const std::string &msg) {
+	if (s->get_resource() == "/getCaseCount") {
+		std::cout << "msg |" << msg.substr(1,msg.size()-2) << "|" << std::endl;
+		m_case_count = atoi(msg.substr(1,msg.size()-2).c_str());
+	} else {
+		s->send(msg);
+	}
+}
+
+void echo_client_handler::on_message(session_ptr s,
+									 const std::vector<unsigned char> &data) {
+	s->send(data);
+}
