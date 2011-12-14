9e14fba2f77c5605de7e72eabd1a4310bd55efd5
Peter Thorson <git@zaphoyd.com>
2011-12-14 15:08:03 -0600

    md5_hash_string now works with arbitrary length strings

--- src/md5/md5.c
+++ src/md5/md5.c
@@ -320,10 +320,10 @@ md5_init(md5_state_t *pms)
 }
 
 void
-md5_append(md5_state_t *pms, const md5_byte_t *data, int nbytes)
+md5_append(md5_state_t *pms, const md5_byte_t *data, size_t nbytes)
 {
     const md5_byte_t *p = data;
-    int left = nbytes;
+    size_t left = nbytes;
     int offset = (pms->count[0] >> 3) & 63;
     md5_word_t nbits = (md5_word_t)(nbytes << 3);
 
--- src/md5/md5.hpp
+++ src/md5/md5.hpp
@@ -29,6 +29,9 @@
 #define WEBSOCKETPP_MD5_WRAPPER_HPP
 
 #include "md5.h"
+#include <string>
+
+#include <iostream>
 
 namespace websocketpp {
 
@@ -39,11 +42,25 @@ inline std::string md5_hash_string(const std::string& s) {
 	md5_state_t state;
 	
 	md5_init(&state);
-	md5_append(&state, (const md5_byte_t *)s.c_str(), 16);
+	md5_append(&state, (const md5_byte_t *)s.c_str(), s.size());
 	md5_finish(&state, (md5_byte_t *)digest);
 	
 	digest[16] = '\0';
 	return std::string(digest);
+}
+
+const char hexval[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+inline std::string md5_hash_hex(const std::string& input) {
+    std::string hash = md5_hash_string(input);
+    std::string hex;
+        
+    for (size_t i = 0; i < hash.size(); i++) {
+        hex.push_back(hexval[((hash[i] >> 4) & 0xF)]);
+        hex.push_back(hexval[(hash[i]) & 0x0F]);
+    }
+    
+    return hex;
 }
 
 } // websocketpp
