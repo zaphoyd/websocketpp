3523052529188b9557f910cec46b067f31f0a7c8
Peter Thorson <github@zaphoyd.com>
2012-01-13 08:16:50 -0600

    server policy delegates calls to run to endpoint

--- src/roles/server.hpp
+++ src/roles/server.hpp
@@ -220,7 +220,7 @@ void server<endpoint>::listen(uint16_t port) {
     
     m_ws_endpoint.alog().at(log::alevel::DEVEL) << "role::server listening on port " << port << log::endl;
     
-    m_io_service.run();
+    m_ws_endpoint.run();
 }
 
 template <class endpoint>
