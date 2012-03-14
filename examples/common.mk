f085cc63b6dffc6563430192b645453c8d3274f8
Sven Almgren <sven@e-kontakt.se>
2012-03-14 12:52:55 +0100

    Centralized makefiles for examples

---
+++ examples/common.mk
@@ -0,0 +1,19 @@
+BOOST_PREFIX ?= /usr/local
+BOOST_LIB_PATH		?= $(BOOST_PREFIX)/lib
+BOOST_INCLUDE_PATH ?= $(BOOST_PREFIX)/include
+
+CPP11               ?= 
+
+CFLAGS = -Wall -O2 $(CPP11) -I$(BOOST_INCLUDE_PATH)
+LDFLAGS = -L$(BOOST_LIB_PATH)
+
+CXX		?= c++
+SHARED  ?= 1
+
+ifeq ($(SHARED), 1)
+	LDFLAGS := $(LDFLAGS) -lwebsocketpp
+	LDFLAGS := $(LDFLAGS) $(BOOST_LIBS:%=-l%)
+else
+	LDFLAGS := $(LDFLAGS) ../../libwebsocketpp.a
+	LDFLAGS := $(LDFLAGS) $(BOOST_LIBS:%=-l$(BOOST_LIB_PATH)/lib%.a)
+endif
