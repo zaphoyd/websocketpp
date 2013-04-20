98171d525efb5b73a576cb789733d9e771c31c60
Peter Thorson <github@zaphoyd.com>
2013-04-20 07:30:52 -0500

    adds C++ namespaces to <cstring> methods references #209

--- websocketpp/md5/md5.hpp
+++ websocketpp/md5/md5.hpp
@@ -202,7 +202,7 @@ md5_process(md5_state_t *pms, const md5_byte_t *data /*[64]*/)
 		X = (const md5_word_t *)data;
 	    } else {
 		/* not aligned */
-		memcpy(xbuf, data, 64);
+		std::memcpy(xbuf, data, 64);
 		X = xbuf;
 	    }
 	}
@@ -376,7 +376,7 @@ md5_append(md5_state_t *pms, const md5_byte_t *data, size_t nbytes)
     if (offset) {
 	int copy = (offset + nbytes > 64 ? 64 - offset : nbytes);
 
-	memcpy(pms->buf + offset, p, copy);
+	std::memcpy(pms->buf + offset, p, copy);
 	if (offset + copy < 64)
 	    return;
 	p += copy;
@@ -390,7 +390,7 @@ md5_append(md5_state_t *pms, const md5_byte_t *data, size_t nbytes)
 
     /* Process a final partial block. */
     if (left)
-	memcpy(pms->buf, p, left);
+	std::memcpy(pms->buf, p, left);
 }
 
 void
