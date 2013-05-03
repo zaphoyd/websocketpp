b6a741ed236126ef78424902590bec95e908a8d0
Peter Thorson <git@zaphoyd.com>
2013-05-03 17:12:51 -0500

    fixes utility client tls settings

--- examples/utility_client/utility_client.cpp
+++ examples/utility_client/utility_client.cpp
@@ -1,17 +1,17 @@
-#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/config/asio_client.hpp>
 
 #include <websocketpp/client.hpp>
 
 #include <iostream>
 
-typedef websocketpp::client<websocketpp::config::asio_client> client;
+typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
 
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 
 // pull out the type of messages sent by our config
-typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+typedef websocketpp::config::asio_tls_client::message_type::ptr message_ptr;
 
 
 void print_handler(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
