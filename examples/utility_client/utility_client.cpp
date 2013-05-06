b2fbb7b8ba57549cea98eddc79cee5d625eb327b
Peter Thorson <github@zaphoyd.com>
2013-05-05 19:09:03 -0500

    registers missing handlers

--- examples/utility_client/utility_client.cpp
+++ examples/utility_client/utility_client.cpp
@@ -32,9 +32,11 @@ public:
         m_endpoint.init_asio();
         
         // Register our handlers
+        m_endpoint.set_socket_init_handler(bind(&type::on_socket_init,this,::_1));
+        m_endpoint.set_tls_init_handler(bind(&type::on_tls_init,this,::_1));
         m_endpoint.set_message_handler(bind(&type::on_message,this,::_1,::_2));
         m_endpoint.set_open_handler(bind(&type::on_open,this,::_1));
-        m_endpoint.set_tls_init_handler(bind(&type::on_tls_init,this,::_1));
+        m_endpoint.set_close_handler(bind(&type::on_close,this,::_1));
     }
     
     void start(std::string uri) {
