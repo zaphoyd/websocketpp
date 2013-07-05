c32bebba3ea6b6e48ee73269650d54ea4d1edd63
Peter Thorson <github@zaphoyd.com>
2013-07-05 11:39:02 -0500

    take over print server example for testing

--- examples/print_server/print_server.cpp
+++ examples/print_server/print_server.cpp
@@ -1,18 +1,60 @@
 #include <iostream>
 
-#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/config/debug_asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
-typedef websocketpp::server<websocketpp::config::asio> server;
+#include <websocketpp/extensions/permessage_deflate/enabled.hpp>
+
+struct deflate_config : public websocketpp::config::debug_core {
+    typedef deflate_config type;
+    typedef debug_core base;
+    
+    typedef base::concurrency_type concurrency_type;
+    
+    typedef base::request_type request_type;
+    typedef base::response_type response_type;
+
+    typedef base::message_type message_type;
+    typedef base::con_msg_manager_type con_msg_manager_type;
+    typedef base::endpoint_msg_manager_type endpoint_msg_manager_type;
+    
+    typedef base::alog_type alog_type;
+    typedef base::elog_type elog_type;
+    
+    typedef base::rng_type rng_type;
+    
+    struct transport_config : public base::transport_config {
+        typedef type::concurrency_type concurrency_type;
+        typedef type::alog_type alog_type;
+        typedef type::elog_type elog_type;
+        typedef type::request_type request_type;
+        typedef type::response_type response_type;
+        typedef websocketpp::transport::asio::basic_socket::endpoint 
+            socket_type;
+    };
+
+    typedef websocketpp::transport::asio::endpoint<transport_config> 
+        transport_type;
+        
+    /// permessage_compress extension
+    struct permessage_deflate_config {};
+
+    typedef websocketpp::extensions::permessage_deflate::enabled
+        <permessage_deflate_config> permessage_deflate_type;
+};
+
+typedef websocketpp::server<deflate_config> server;
 
 void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
-        std::cout << msg->get_payload() << std::endl;
+        std::cout << "on_message: " << msg->get_payload() << std::endl;
 }
 
 int main() {
     server print_server;
 
     print_server.set_message_handler(&on_message);
+    print_server.set_access_channels(websocketpp::log::alevel::all);
+    print_server.set_error_channels(websocketpp::log::elevel::all);
 
     print_server.init_asio();
     print_server.listen(9002);
