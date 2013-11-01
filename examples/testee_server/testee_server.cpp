f6fd37ae0508a72fb2e1a2a1c3cf4a5944272e89
Peter Thorson <github@zaphoyd.com>
2013-11-01 16:39:52 -0500

    reset some defaults

--- examples/testee_server/testee_server.cpp
+++ examples/testee_server/testee_server.cpp
@@ -63,19 +63,21 @@ struct testee_config : public websocketpp::config::asio {
     typedef core::concurrency_type concurrency_type;
     typedef core::request_type request_type;
     typedef core::response_type response_type;
-    //typedef core::message_type message_type;
-    //typedef core::con_msg_manager_type con_msg_manager_type;
-    //typedef core::endpoint_msg_manager_type endpoint_msg_manager_type;
-    typedef websocketpp::message_buffer::fixed::policy::message message_type;
-    typedef websocketpp::message_buffer::fixed::policy::con_msg_manager con_msg_manager_type;
-    //typedef websocketpp::message_buffer::fixed::policy::con_msg_manager endpoint_msg_manager_type;
-    typedef websocketpp::message_buffer::fixed::policy::endpoint_msg_manager endpoint_msg_manager_type;
+    typedef core::message_type message_type;
+    typedef core::con_msg_manager_type con_msg_manager_type;
+    typedef core::endpoint_msg_manager_type endpoint_msg_manager_type;
+
+    //typedef websocketpp::message_buffer::fixed::policy::message message_type;
+    //typedef websocketpp::message_buffer::fixed::policy::con_msg_manager con_msg_manager_type;
+    ////typedef websocketpp::message_buffer::fixed::policy::con_msg_manager endpoint_msg_manager_type;
+    //typedef websocketpp::message_buffer::fixed::policy::endpoint_msg_manager endpoint_msg_manager_type;
+
     typedef core::alog_type alog_type;
     typedef core::elog_type elog_type;
     typedef core::rng_type rng_type;
     typedef core::endpoint_base endpoint_base;
 
-    static bool const enable_multithreading = false;
+    static bool const enable_multithreading = true;
 
     struct transport_config : public core::transport_config {
         typedef core::concurrency_type concurrency_type;
@@ -84,7 +86,7 @@ struct testee_config : public websocketpp::config::asio {
         typedef core::request_type request_type;
         typedef core::response_type response_type;
 
-        static bool const enable_multithreading = false;
+        static bool const enable_multithreading = true;
     };
 
     typedef websocketpp::transport::asio::endpoint<transport_config>
