19f7886ca4f2343648ea78da4f99c0e9163c9322
Peter Thorson <github@zaphoyd.com>
2014-11-04 12:23:30 -0500

    reset dev branch versions to dev status

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-WebSocket++ (0.4.0)
+WebSocket++ (0.4.x-dev)
 ==========================
 
 WebSocket++ is a header only C++ library that implements RFC6455 The WebSocket
--- websocketpp/version.hpp
+++ websocketpp/version.hpp
@@ -50,10 +50,10 @@ static int const patch_version = 0;
  * This is a textual flag indicating the type and number for pre-release
  * versions (dev, alpha, beta, rc). This will be blank for release versions.
  */
-static char const prerelease_flag[] = "";
+static char const prerelease_flag[] = "dev";
 
 /// Default user agent string
-static char const user_agent[] = "WebSocket++/0.4.0";
+static char const user_agent[] = "WebSocket++/0.4.x-dev";
 
 } // namespace websocketpp
 
