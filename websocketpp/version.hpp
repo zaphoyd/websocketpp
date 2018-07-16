c6d7e295bf5a0ab9b5f896720cc1a0e0fdc397a7
Peter Thorson <git@zaphoyd.com>
2018-07-16 07:40:53 -0500

    Set version flags for release

--- websocketpp/version.hpp
+++ websocketpp/version.hpp
@@ -44,17 +44,17 @@ static int const major_version = 0;
 /// Library minor version number
 static int const minor_version = 8;
 /// Library patch version number
-static int const patch_version = 0;
+static int const patch_version = 1;
 /// Library pre-release flag
 /**
  * This is a textual flag indicating the type and number for pre-release
  * versions (dev, alpha, beta, rc). This will be blank for release versions.
  */
 
-static char const prerelease_flag[] = "dev";
+static char const prerelease_flag[] = "";
 
 /// Default user agent string
-static char const user_agent[] = "WebSocket++/0.9.0-dev";
+static char const user_agent[] = "WebSocket++/0.8.1";
 
 } // namespace websocketpp
 
