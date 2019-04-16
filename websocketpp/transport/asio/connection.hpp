2c57b6fbf03a47dbcfe51491411eb63c8946a4f3
Stefan Floeren <42731906+stefan-floeren@users.noreply.github.com>
2019-04-16 08:38:01 +0200

    Replace make_shared with new in some cases
    Replace make_shared for asio types that take a lib::ref as a parameter.
    This should fix the ASIO change (boostorg/asio@59066d8) for 1.70,
    while keeping it backwards compatible to older boost versions.

--- websocketpp/transport/asio/connection.hpp
+++ websocketpp/transport/asio/connection.hpp
@@ -311,9 +311,10 @@ public:
      * needed.
      */
     timer_ptr set_timer(long duration, timer_handler callback) {
-        timer_ptr new_timer = lib::make_shared<lib::asio::steady_timer>(
-            lib::ref(*m_io_service),
-            lib::asio::milliseconds(duration)
+        timer_ptr new_timer(
+            new lib::asio::steady_timer(
+                *m_io_service,
+                lib::asio::milliseconds(duration))
         );
 
         if (config::enable_multithreading) {
